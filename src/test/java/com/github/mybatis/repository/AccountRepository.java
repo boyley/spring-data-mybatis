2e193c0c6c6dd69b64de5be1bdfd066a08f5c10c
Bogle <136397156@qq.com>
2016-09-08 13:29:56 +0800

    修改完成

--- src/test/java/com/github/mybatis/repository/AccountRepository.java
+++ src/test/java/com/github/mybatis/repository/AccountRepository.java
@@ -3,6 +3,8 @@ package com.github.mybatis.repository;
 import com.github.mybatis.domains.Account;
 import com.github.mybatis.repository.support.MybatisRepository;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Created by Bogle on 2016/9/7.
@@ -11,4 +13,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface AccountRepository extends MybatisRepository<Account, Long> {
 
     Account selectByPrimaryKey(Integer id);
+
+    Page<Account> findAll(Pageable pageable);
 }
--- src/test/java/com/github/mybatis/test/MybatisRepositoryTest.java
+++ src/test/java/com/github/mybatis/test/MybatisRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.github.mybatis.test;
 
 import com.github.mybatis.repository.AccountRepository;
+import com.github.mybatis.repository.config.EnableMybatisRepositories;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -12,10 +14,18 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableMybatisRepositories(
+        basePackages = {"com.github.mybatis.repository"}
+)
 public class MybatisRepositoryTest {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Test
+    public void findPage() {
+        accountRepository.findAll(new PageRequest(1,1));
+    }
 
     @Test
     public void selectByPrimaryKey() {
